ee9e1b368ca6205e20e19e5e228a9ec40a84c2a0
tbou30897.msj <80086264+tbou30897@users.noreply.github.com>
2022-12-01 18:00:05 +0900

    Add files via upload

---
+++ RTC/SNS system components/Twitter_API/test/twitter_apiTest.py
@@ -0,0 +1,294 @@
+﻿#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+# -*- Python -*-
+
+# <rtc-template block="description">
+"""
+ @file twitter_apiTest.py
+ @brief This component retrieves tweets (latest tweets) related to specified keywords from twitter.
+ @date $Date$
+
+
+"""
+# </rtc-template>
+
+from __future__ import print_function
+import sys
+import time
+sys.path.append(".")
+
+# Import RTM module
+import RTC
+import OpenRTM_aist
+
+
+# Import Service implementation class
+# <rtc-template block="service_impl">
+
+import twitter_api
+
+# </rtc-template>
+
+# Import Service stub modules
+# <rtc-template block="consumer_import">
+# </rtc-template>
+
+
+# This module's spesification
+# <rtc-template block="module_spec">
+twitter_apitest_spec = ["implementation_id", "twitter_apiTest", 
+         "type_name",         "twitter_apiTest", 
+         "description",       "This component retrieves tweets (latest tweets) related to specified keywords from twitter.", 
+         "version",           "1.0.0", 
+         "vendor",            "tbou30897", 
+         "category",          "Information_acquisition", 
+         "activity_type",     "STATIC", 
+         "max_instance",      "1", 
+         "language",          "Python", 
+         "lang_type",         "SCRIPT",
+         ""]
+# </rtc-template>
+
+# <rtc-template block="component_description">
+##
+# @class twitter_apiTest
+# @brief This component retrieves tweets (latest tweets) related to specified keywords from twitter.
+# 
+# 
+# </rtc-template>
+class twitter_apiTest(OpenRTM_aist.DataFlowComponentBase):
+    
+    ##
+    # @brief constructor
+    # @param manager Maneger Object
+    # 
+    def __init__(self, manager):
+        OpenRTM_aist.DataFlowComponentBase.__init__(self, manager)
+
+        self._d_TextOut = OpenRTM_aist.instantiateDataType(RTC.TimedWStringSeq)
+        """
+        本コンポーネントが取得したデータをリスト型で送信するデータポートです。
+         - Type: TimedWStrintSeq
+         - Semantics: [‘ツイート 1’,’ ツイート 2’,’ ツイート 3’,’ ツイート
+		              4’]
+		              ↑のように検索ワードに関するツイートをリスト型で出力
+        """
+        self._TextOutIn = OpenRTM_aist.InPort("TextOut", self._d_TextOut)
+
+
+        
+
+
+        # initialize of configuration-data.
+        # <rtc-template block="init_conf_param">
+        
+        # </rtc-template>
+
+
+         
+    ##
+    #
+    # The initialize action (on CREATED->ALIVE transition)
+    # 
+    # @return RTC::ReturnCode_t
+    # 
+    #
+    def onInitialize(self):
+        # Bind variables and configuration variable
+        
+        # Set InPort buffers
+        self.addInPort("TextOut",self._TextOutIn)
+        
+        # Set OutPort buffers
+        
+        # Set service provider to Ports
+        
+        # Set service consumers to Ports
+        
+        # Set CORBA Service Ports
+        
+        return RTC.RTC_OK
+    
+    ###
+    ## 
+    ## The finalize action (on ALIVE->END transition)
+    ## 
+    ## @return RTC::ReturnCode_t
+    #
+    ## 
+    #def onFinalize(self):
+    #
+    #    return RTC.RTC_OK
+    
+    #    ##
+    ##
+    ## The startup action when ExecutionContext startup
+    ## 
+    ## @param ec_id target ExecutionContext Id
+    ##
+    ## @return RTC::ReturnCode_t
+    ##
+    ##
+    #def onStartup(self, ec_id):
+    #
+    #    return RTC.RTC_OK
+    
+    ###
+    ##
+    ## The shutdown action when ExecutionContext stop
+    ##
+    ## @param ec_id target ExecutionContext Id
+    ##
+    ## @return RTC::ReturnCode_t
+    ##
+    ##
+    #def onShutdown(self, ec_id):
+    #
+    #    return RTC.RTC_OK
+    
+    ##
+    #
+    # The activated action (Active state entry action)
+    #
+    # @param ec_id target ExecutionContext Id
+    # 
+    # @return RTC::ReturnCode_t
+    #
+    #
+    def onActivated(self, ec_id):
+    
+        return RTC.RTC_OK
+    
+        ##
+    #
+    # The deactivated action (Active state exit action)
+    #
+    # @param ec_id target ExecutionContext Id
+    #
+    # @return RTC::ReturnCode_t
+    #
+    #
+    def onDeactivated(self, ec_id):
+    
+        return RTC.RTC_OK
+    
+    ##
+    #
+    # The execution action that is invoked periodically
+    #
+    # @param ec_id target ExecutionContext Id
+    #
+    # @return RTC::ReturnCode_t
+    #
+    #
+    def onExecute(self, ec_id):
+    
+        return RTC.RTC_OK
+    
+    ###
+    ##
+    ## The aborting action when main logic error occurred.
+    ##
+    ## @param ec_id target ExecutionContext Id
+    ##
+    ## @return RTC::ReturnCode_t
+    #    #
+    ##
+    #def onAborting(self, ec_id):
+    #
+    #    return RTC.RTC_OK
+    
+    ###
+    ##
+    ## The error action in ERROR state
+    ##
+    ## @param ec_id target ExecutionContext Id
+    ##
+    ## @return RTC::ReturnCode_t
+    ##
+    ##
+    #def onError(self, ec_id):
+    #
+    #    return RTC.RTC_OK
+    
+    ###
+    ##
+    ## The reset action that is invoked resetting
+    ##
+    ## @param ec_id target ExecutionContext Id
+    ##
+    ## @return RTC::ReturnCode_t
+    ##
+    ##
+    #def onReset(self, ec_id):
+    #
+    #    return RTC.RTC_OK
+    
+    ###
+    ##
+    ## The state update action that is invoked after onExecute() action
+    ##
+    ## @param ec_id target ExecutionContext Id
+    ##
+    ## @return RTC::ReturnCode_t
+    ##
+
+    ##
+    #def onStateUpdate(self, ec_id):
+    #
+    #    return RTC.RTC_OK
+    
+    ###
+    ##
+    ## The action that is invoked when execution context's rate is changed
+    ##
+    ## @param ec_id target ExecutionContext Id
+    ##
+    ## @return RTC::ReturnCode_t
+    ##
+    ##
+    #def onRateChanged(self, ec_id):
+    #
+    #    return RTC.RTC_OK
+    
+    def runTest(self):
+        return True
+
+def RunTest():
+    manager = OpenRTM_aist.Manager.instance()
+    comp = manager.getComponent("twitter_apiTest0")
+    if comp is None:
+        print('Component get failed.', file=sys.stderr)
+        return False
+    return comp.runTest()
+
+def twitter_apiTestInit(manager):
+    profile = OpenRTM_aist.Properties(defaults_str=twitter_apitest_spec)
+    manager.registerFactory(profile,
+                            twitter_apiTest,
+                            OpenRTM_aist.Delete)
+
+def MyModuleInit(manager):
+    twitter_apiTestInit(manager)
+    twitter_api.twitter_apiInit(manager)
+
+    # Create a component
+    comp = manager.createComponent("twitter_apiTest")
+
+def main():
+    mgr = OpenRTM_aist.Manager.init(sys.argv)
+    mgr.setModuleInitProc(MyModuleInit)
+    mgr.activateManager()
+    mgr.runManager(True)
+
+    ret = RunTest()
+    mgr.shutdown()
+
+    if ret:
+        sys.exit(0)
+    else:
+        sys.exit(1)
+
+if __name__ == "__main__":
+    main()
+
